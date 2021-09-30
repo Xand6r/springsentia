33fc3b4e41c94f7d8cff6e93a18fcbaadf69e1e7
xander <shuaibualexander@gmail.com>
2021-10-01 00:06:03 +0100

    finalise secind section

--- pages/home/components/process/index.jsx
+++ pages/home/components/process/index.jsx
@@ -1,6 +1,9 @@
 import Image from 'next/image';
 import {
-    Nature
+    Nature,
+    Formulation,
+    Science,
+    Beauty
 } from './assets';
 import styles from './process.module.scss';
 
@@ -16,15 +19,32 @@ export default function Index() {
 
             <div className={styles.process__tabcontent}>
                 <div className={styles.tab__group}>
-                    <Tab ImageItem={Nature} heading="Inspired by nature" text="We believe that nature has a lot to offer when it comes to beauty. So through partnerships with local communities across Africa, we are able to utilize natural ingredients to make products that are nourishing." />
+                    <Tab
+                        ImageItem={Nature}
+                        heading="Inspired by nature"
+                        text="We believe that nature has a lot to offer when it comes to beauty. So through partnerships with local communities across Africa, we are able to utilize natural ingredients to make products that are nourishing."
+                    />
                     <div className={styles.vertical__line} />
-                    <Tab ImageItem={Nature} heading="Inspired by nature" text="We believe that nature has a lot to offer when it comes to beauty. So through partnerships with local communities across Africa, we are able to utilize natural ingredients to make products that are nourishing." />
+                    <Tab
+                        ImageItem={Beauty}
+                        heading="Beauty made to last"
+                        text="Quick fixes never last. So at Springsentia, we are committed to making products that deliver gradual but long-lasting results without compromise."
+                    />
                 </div>
                 <div className={styles.horizontal__line} />
                 <div className={styles.tab__group}>
-                    <Tab ImageItem={Nature} heading="Inspired by nature" text="We believe that nature has a lot to offer when it comes to beauty. So through partnerships with local communities across Africa, we are able to utilize natural ingredients to make products that are nourishing." />
+                    <Tab
+                        ImageItem={Formulation}
+                        heading="Simple formulations"
+                        text="We use potent and multifunctional ingredients to make simple formulations that deliver effective results that your hair and skin will love. We focus on what we put into the products rather than
+                        what we leave out."
+                    />
                     <div className={styles.vertical__line} />
-                    <Tab ImageItem={Nature} heading="Inspired by nature" text="We believe that nature has a lot to offer when it comes to beauty. So through partnerships with local communities across Africa, we are able to utilize natural ingredients to make products that are nourishing." />
+                    <Tab
+                        ImageItem={Science}
+                        heading="Driven by science"
+                        text="Quick fixes never last. So at Springsentia, we are committed to making products that deliver gradual but long-lasting results without compromise."
+                    />
                 </div>
             </div>
         </section>
