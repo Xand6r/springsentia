98637fdd536b37e01b6cba5abb1bd9cf6278be3a
xander <shuaibualexander@gmail.com>
2021-10-01 10:53:00 +0100

    integrate forms

--- pages/home/components/landing/index.jsx
+++ pages/home/components/landing/index.jsx
@@ -2,8 +2,13 @@ import Image from "next/image";
 
 import LeftArrow from "/public/images/leftarrow.svg";
 import styles from "./landing.module.scss";
+import { useForm } from "@formspree/react";
+process.env.NEXT_PUBLIC_REGISTERATION = "mgerqadb";
 
 export default function Index() {
+  const [state, handleSubmit] = useForm(process.env.NEXT_PUBLIC_REGISTERATION);
+  const submittedStyle = state.succeeded ? { visibility: 'hidden' } : {};
+  const submittedText = state.succeeded ? "Thank you! we will be in touch" : "Get notified when we launch";
   return (
     <section className={styles.landing__cover}>
       <div className={styles.landing__content}>
@@ -13,17 +18,36 @@ export default function Index() {
           too. That’s why we’ve put in so much care in making products that give
           you real &amp; natural results.
         </h4>
-        <h3>Get notified when we launch</h3>
-        <div className={styles.input__group}>
-            <input
-                type="text"
-                placeholder="Enter email"
-            />
-            <button className="--filled">
-                <span>GET NOTIFIED</span>
-                <Image alt="left arrow" src={LeftArrow}/>
-            </button>
-        </div>
+        <h3>{submittedText}</h3>
+        <form
+          style={submittedStyle}
+          onSubmit={handleSubmit}
+          className={styles.input__group}
+        >
+          <input
+            id="email"
+            type="email"
+            name="email"
+            type="text"
+            placeholder="Enter email"
+          />
+          <button
+            type="submit"
+            disabled={state.submitting}
+            className="--filled"
+          >
+            {
+              !state.submitting ? (
+                <>
+                  <span>GET NOTIFIED</span>
+                  <Image alt="left arrow" src={LeftArrow} />
+                </>
+              ) : (
+                <span>SAVING ...</span>
+              )
+            }
+          </button>
+        </form>
       </div>
     </section>
   );
