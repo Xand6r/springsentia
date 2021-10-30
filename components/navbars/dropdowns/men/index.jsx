e1fba144bce263fb7f71afda371983231f09f9fc
xander <shuaibualexander@gmail.com>
2021-10-30 14:01:33 +0100

    create initial header

---
+++ components/navbars/dropdowns/men/index.jsx
@@ -0,0 +1,10 @@
+import generalStyles from '../dropdown.modules.scss';
+import styles from './dropdown.modules.scss';
+
+export default function Index() {
+    return (
+        <div>
+            
+        </div>
+    )
+}
