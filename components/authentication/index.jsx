639b33bdaaaec695cbb92b51d826d7fe827bff63
xander <shuaibualexander@gmail.com>
2021-12-12 20:30:05 +0100

    fix confirmation mode

--- components/authentication/index.jsx
+++ components/authentication/index.jsx
@@ -1,44 +1,56 @@
-import { useState } from "react";
-import styles from "./authentication.module.scss";
+import { useState, useEffect } from "react";
+import { useRouter } from "next/router";
 
+import styles from "./authentication.module.scss";
 import Register from "./components/register";
 import Login from "./components/login";
 
+const KEY = "confirmed";
+
 export default function Index() {
+  const router = useRouter();
   const [activeTab, setActiveTab] = useState(1);
+  const [confirmationMode, setConfirmationMode] = useState(false);
+
+  useEffect(() => {
+    if (router.query.hasOwnProperty(KEY) && !confirmationMode) {
+      setActiveTab(2);
+      setConfirmationMode(true);
+    }
+  }, [router.query]);
+
   return (
     <div className={styles.authcomponent}>
       {/* the heading tab of the login component */}
-      <div className={styles.heading}>
-        <div
-          data-active={activeTab === 1 && "active"}
-          onClick={() => setActiveTab(1)}
-        >
-          Register
+      {!confirmationMode && (
+        <div className={styles.heading}>
+          <div
+            data-active={activeTab === 1 && "active"}
+            onClick={() => setActiveTab(1)}
+          >
+            Register
+          </div>
+          <div
+            data-active={activeTab === 2 && "active"}
+            onClick={() => setActiveTab(2)}
+          >
+            Login
+          </div>
         </div>
-        <div
-          data-active={activeTab === 2 && "active"}
-          onClick={() => setActiveTab(2)}
-        >
-          Login
-        </div>
-      </div>
+      )}
       {/* the heading tab of the login component */}
 
       {/* the content of the login component */}
       <div className={styles.content}>
-        {
-          activeTab === 1 ? (
-            // {/* tab one for register */}
-            <Register />
-            // {/* tab one for register */}
-          ) : (
-            // {/* tab two for login*/}
-            <Login />
-            // {/* tab two for login*/}
-          )
-        }
-        
+        {activeTab === 1 ? (
+          // {/* tab one for register */}
+          <Register />
+        ) : (
+          // {/* tab one for register */}
+          // {/* tab two for login*/}
+          <Login confirmationMode={confirmationMode} />
+          // {/* tab two for login*/}
+        )}
       </div>
       {/* the content of the login component */}
     </div>
